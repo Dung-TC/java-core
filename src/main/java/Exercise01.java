be5c28356c04de4cc764fa729df1d28849db2a7b
Dung-TC <truongdungkyoto9999@gmail.com>
2024-05-07 00:12:38 +0900

    update Assignment-02

--- src/main/java/Exercise01.java
+++ src/main/java/Exercise01.java
@@ -1,2 +1,16 @@
 public class Exercise01 {
+    void question01() {
+        Department department = new Department();
+        department.name = "Bảo vệ";
+
+        Account account = new Account();
+        account.department = department;
+
+        if (account.department == null){
+            System.out.println("Không có phòng ban");
+        } else {
+            System.out.println("phong ban của " + account.userName " là " + department.name);
+        }
+
+    }
 }
--- src/main/java/Program01.java
+++ src/main/java/Program01.java
@@ -1,2 +1,5 @@
-package PACKAGE_NAME;public class Program01 {
+public class Program01 {
+    public static void main(String[] args) {
+
+    }
 }
