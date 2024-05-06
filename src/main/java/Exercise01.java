319d4c5be7140c30a84340c107da305e32c99c8a
Dung-TC <truongdungkyoto9999@gmail.com>
2024-05-07 00:27:04 +0900

    update Assignment-02 Bai mau cua thay

--- src/main/java/Exercise01.java
+++ src/main/java/Exercise01.java
@@ -6,11 +6,23 @@ public class Exercise01 {
         Account account = new Account();
         account.department = department;
 
-        if (account.department == null){
-            System.out.println("Không có phòng ban");
+        if (account.department == null) {
+            System.out.println("Nhân viên này chưa có phòng ban");
         } else {
-            System.out.println("phong ban của " + account.userName " là " + department.name);
+            System.out.println("Phòng ban của nhân viên này là: " + account.department.name);
         }
+    }
 
+    void question03() {
+        Department department = new Department();
+        department.name = "Bảo vệ";
+
+        Account account = new Account();
+        account.department = department;
+
+        String message = (account.department == null)
+                ? "Nhân viên này chưa có phòng ban"
+                : "Phòng ban của nhân viên này là: " + account.department.name;
+        System.out.println(message);
     }
 }
