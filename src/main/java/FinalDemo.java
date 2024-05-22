02039ce1e11c4e5d93e5354a7140a7b6afea23f7
Dung-TC <truongdungkyoto9999@gmail.com>
2024-05-22 22:14:38 +0900

    less-09

---
+++ src/main/java/FinalDemo.java
@@ -0,0 +1,13 @@
+public class FinalDemo {
+    public static void main(String[] args) {
+        //Tạo hằng số
+        final double PI = 3.14 ;
+
+        Dog dog = new Dog();
+        dog.eat();
+
+        //final class
+        //  Đánh dấu class đó không có class con, ớp con không thể kế thừa class
+
+    }
+}
---
+++ src/main/java/StaticDemo.java
@@ -0,0 +1,24 @@
+public class StaticDemo {
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        // static: thuộc về class
+        Student s1 = new Student("khoa" , 20);
+        Student s2 = new Student("Hung", 15);
+
+        s1.count = 10;
+        System.out.println("s2.count = " + s2.count);
+
+        // truy cập thông qua tên của class
+        System.out.println("Student.count = " + Student.count);
+
+        int max = Student.max(10,100);
+        System.out.println("max = " + max);
+
+        // Static không thể truy cập non-static(cá nhân)
+        // non-static có thể truy câp static
+
+        Math.PI
+
+
+    }
+}
