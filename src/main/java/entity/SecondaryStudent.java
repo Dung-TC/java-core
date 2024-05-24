d7971ea161e2990d9573093db4d45739c6344e29
Dung-TC <truongdungkyoto9999@gmail.com>
2024-05-24 21:43:24 +0900

    24/5/2024

---
+++ src/main/java/entity/SecondaryStudent.java
@@ -0,0 +1,10 @@
+package entity;
+
+public class PrimaryStudent extends Student {
+    public static int count;
+    public PrimaryStudent(String namee){
+        super(name);
+        count++;
+
+    }
+}
